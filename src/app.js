a8b7e65e6e83855ecca23b3b18a098cb9a756628
Tan Moy <tanmoy.pto@gmail.com>
2017-04-18 22:30:13 +0530

    added webdev server and config options

--- src/app.js
+++ src/app.js
@@ -1,3 +1,3 @@
 const css = require('./app.scss'); //tis tells webpack to include css in bundle.js. We will need styles loader to actually apply the css
 
-console.log('hola from bundle.app.js')
+console.log('hola from bundle.app.js amd webpack dev server')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin'); //creates a dynamic html file each time webpack runs, plugin can be customized below
-const ExtractTextPlugin = require("extract-text-webpack-plugin");//It moves all the require("style.css")s in entry chunks into a separate single CSS file.
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //creates a dynamic html file each time webpack runs, plugin can be customized below
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin"); //It moves all the require("style.css")s in entry chunks into a separate single CSS file.
+
 module.exports = {
     entry: "./src/app.js", //input to webpack
     output: {
@@ -13,24 +15,34 @@ module.exports = {
         rules: [{
             //checks for any .scss files and use css-loader to include it in bundle.js and style-loader to make the css work. Webpack looks at these files from bottom up. ExtractTextPlugin to eject them all into a separate file
             test: /\.scss/,
-            //use: ["style-loader","css-loader","sass-loader"] //old use, modified below to be used with ExtractTextPlugin
+            //use: ["style-loader","css-loader","sass-loader"]
+            // ^ old use, modified below to be used with ExtractTextPlugin
             use: ExtractTextPlugin.extract({
                 fallback: "style-loader",
-                use: ["css-loader","sass-loader"]
+                use: ["css-loader", "sass-loader"]
             })
         }]
-},
+    },
 
-plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Project',
-        minify: {
-            //minify index.html
-            collapseWhitespace: true
-        },
+    devServer: {
+        contentBase: __dirname + '/dist', //location from which everything is served
+        compress: true, // Set this if you want to enable gzip compression for assets
+        hot: true, // Enable special support for Hot Module Replacement
+        //port:8080, //set port
+        stats:"errors-only"// shows errors only in terminal
 
-        template: './src/index.ejs', //this template will be used to create index.html by HtmlWebpackPlugin. Should not contain links to stylesheets or js files
-    }),
-    new ExtractTextPlugin("app.css"),
-]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project',
+            // minify: {
+            //     //uncomment to  minify index.html
+            //     collapseWhitespace: true
+            // },
+
+            template: './src/index.ejs', //this template will be used to create index.html by HtmlWebpackPlugin. Should not contain links to stylesheets or js files
+        }),
+        new ExtractTextPlugin("app.css"), // Create a new file called app.css in dist wit all the styles
+    ]
 }
